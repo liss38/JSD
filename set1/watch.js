8d050f8938c90f44a419485060d70a4e737e6f9f
Max Salnikov <liss38@yandex.ru>
2018-05-11 17:51:14 +0300

    nojo:: очистил master

--- set1/watch.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-//
-const watcher = fs.watch(__dirname, (event, filename) => {
-  console.log(event);
-  console.log(filename);
-});
-
-watcher.on('error', error => console.log(error));
